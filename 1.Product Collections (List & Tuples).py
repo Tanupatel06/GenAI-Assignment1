daa7114fb4aeccec51bdb7220cd241f24ddaa44a
TANU PATEL <pateltanu672006@gmail.com>
2026-07-14 00:01:41 +0530

    Add files via upload

---
+++ 1.Product Collections (List & Tuples).py
@@ -0,0 +1,9 @@
+products = ["notebook","pen","pencil","eraser","marker","wireless mouse"]
+print(products)
+sample_product = ("product_name: notebook , price: $60 , category: stationery")
+print(sample_product)
+print(products[0])
+print(products[5])
+products.append("Smart Watch")
+products.append("Speaker")
+print(products)
